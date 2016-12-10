c872a3df1cec1ecfa6e380c3be1e3c370cda8bbe
Brian Van Stone <brianvanstone@gmail.com>
2016-12-09 20:44:16 -0500

    Updating package info

--- src/main/java/tech/notpaper/go/messaging/entities/package-info.java
+++ src/main/java/tech/notpaper/go/messaging/entities/package-info.java
@@ -1,5 +1,5 @@
 /**
- * A package designed for holding entities related to the GoParser
+ * A package designed for holding entities related to the GoParser. More specifically these entities are GTP v2 compliant.
  */
 /**
  * @author Brian
