b0d11c9b4e08b4789da1b5887ce2e3c611e53e77
Brian Van Stone <brianvanstone@gmail.com>
2016-12-10 15:14:48 -0500

    Clearing more compiler warnings

--- src/main/java/tech/notpaper/go/messaging/entities/simple/SimpleEntity.java
+++ src/main/java/tech/notpaper/go/messaging/entities/simple/SimpleEntity.java
@@ -5,9 +5,9 @@ import tech.notpaper.go.messaging.entities.complex.ListEntity;
 public interface SimpleEntity extends ListEntity {
 	public static boolean isSimpleEntity(String s) {
 		try {
-			//in order to determine if it is a SimpleBoardEntity,
+			//in order to determine if a String can be a SimpleEntity,
 			//we need only know that it can be parsed as a GoString
-			GoString testString = new GoString(s);
+			new GoString(s);
 			return true;
 		} catch (IllegalArgumentException e) {
 			return false;
